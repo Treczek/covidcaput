e60751443ca61855c20cabe8cb07bb933167a076
Tomasz Reczek <RECZET01@heiway.net>
2021-03-18 14:00:48 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='anti_covid_receipe',
+      version='1.0.0',
+      description='This package should help cure covid.',
+      author='GŻ DS Team',
+      author_email='tomasz.reczek@grupazywiec.pl'
+    )
